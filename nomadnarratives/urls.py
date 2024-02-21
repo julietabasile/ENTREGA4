c5a4ebc96de61ec6acb042b8a127876fea64ee9c
julietabasile <juli.basile1@gmail.com>
2024-02-21 01:14:10 -0300

    se agregan nuevos cambios

--- nomadnarratives/urls.py
+++ nomadnarratives/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),  
+    path('admin/', admin.site.urls),  # URL para el panel de administración
+    path('', include('pages.urls')),   # URLs de la aplicación "pages"
+    
+
 ]
